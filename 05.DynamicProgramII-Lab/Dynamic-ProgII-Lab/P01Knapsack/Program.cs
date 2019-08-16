cfa12b982f1919c5220956aa417fbf0d0b822e0e
hristocr <hristocr@gmail.com>
2019-08-16 20:34:08 +0300

    DP lab finished.

--- 05.DynamicProgramII-Lab/Dynamic-ProgII-Lab/P01Knapsack/Program.cs
+++ 05.DynamicProgramII-Lab/Dynamic-ProgII-Lab/P01Knapsack/Program.cs
@@ -1,12 +1,135 @@
-﻿namespace P01Knapsack
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.ConstrainedExecution;
+
+namespace P01Knapsack
 {
     internal class Program
     {
         public static void Main(string[] args)
         {
-            
-            
-            
+            var maxCapacity = int.Parse(Console.ReadLine());
+
+            var items = ReadItems();
+
+            var maxPrice = new int[items.Count + 1, maxCapacity + 1];
+            var isItemTaken = new bool[items.Count + 1, maxCapacity + 1];
+
+            FillKnapsack(items, maxCapacity, maxPrice, isItemTaken);
+
+            var maxTotalValue = maxPrice[maxPrice.GetLength(0) - 1, maxPrice.GetLength(1) - 1];
+
+            var itemsTaken = ReconstructTakenItems(items, maxPrice, isItemTaken);
+
+            Console.WriteLine($"Total Weight: {itemsTaken.Sum(i => i.Weight)}");
+            Console.WriteLine($"Total Value: {maxTotalValue}");
+            foreach (var item in itemsTaken)
+            {
+                Console.WriteLine(item.Name);
+            }
+        }
+
+        private static void FillKnapsack(IList<Item> items, int maxCapacity, int[,] maxPrice, bool[,] isItemTaken)
+        {
+            // filling max price matrix
+            for (int row = 0; row < maxPrice.GetLength(0); row++)
+            {
+                for (int col = 0; col < maxPrice.GetLength(1); col++)
+                {
+                    if (row == 0)
+                    {
+                        maxPrice[row, col] = 0;
+                        continue;
+                    }
+
+                    var item = items[row - 1];
+                    var currCapacity = col;
+                    var valueExcluding = maxPrice[row - 1, col];
+                    if (item.Weight > currCapacity)
+                    {
+                        // taking best previous max price if can not put item into knapsack
+                        maxPrice[row, col] = valueExcluding;
+                    }
+                    else
+                    {
+                        var valueIncluding = item.Price + maxPrice[row - 1, col - item.Weight];
+                        if (valueExcluding >= valueIncluding)
+                        {
+                            maxPrice[row, col] = valueExcluding;
+                        }
+                        else
+                        {
+                            maxPrice[row, col] = valueIncluding;
+                            isItemTaken[row, col] = true;
+                        }
+                    }
+                }
+            }
+        }
+
+        private static IList<Item> ReconstructTakenItems(IList<Item> items, int[,] maxPrice, bool[,] isItemTaken)
+        {
+            var itemsTaken = new List<Item>();
+
+            var row = maxPrice.GetLength(0) - 1;
+            var col = maxPrice.GetLength(1) - 1;
+
+            var totalPrice = maxPrice[row, col];
+
+            while (totalPrice > 0)
+            {
+                if (isItemTaken[row, col])
+                {
+                    var item = items[row - 1];
+                    itemsTaken.Add(item);
+                    totalPrice -= item.Price;
+                    col -= item.Weight;
+                }
+                row--;
+                
+            }
+
+            itemsTaken = itemsTaken.OrderBy(i=>i.Name).ToList();
+            return itemsTaken;
+        }
+
+        private static IList<Item> ReadItems()
+        {
+            var items = new List<Item>();
+
+            while (true)
+            {
+                var input = Console.ReadLine();
+
+                if (input.Equals("end"))
+                {
+                    break;
+                }
+
+                var itemArr = input.Split(' ');
+
+                var item = new Item(itemArr[0], int.Parse(itemArr[1]), int.Parse(itemArr[2]));
+                items.Add(item);
+            }
+
+            return items;
+        }
+    }
+
+    class Item
+    {
+        public string Name { get; }
+
+        public int Weight { get; }
+
+        public int Price { get; }
+
+        public Item(string name, int weight, int price)
+        {
+            Name = name;
+            Weight = weight;
+            Price = price;
         }
     }
 }
--- 05.DynamicProgramII-Lab/Dynamic-ProgII-Lab/P02LCS/Program.cs
+++ 05.DynamicProgramII-Lab/Dynamic-ProgII-Lab/P02LCS/Program.cs
@@ -1,9 +1,101 @@
-﻿namespace P02LCS
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Common;
+
+namespace P02LCS
 {
     internal class Program
     {
         public static void Main(string[] args)
         {
+            var firstStr = Console.ReadLine();
+            var secondStr = Console.ReadLine();
+
+            var lcs = CalculateLCS(firstStr, secondStr);
+
+            var commonChars = ReconstructLCS(lcs, firstStr, secondStr);
+
+            Console.WriteLine(commonChars.Count);
+        }
+
+        private static int[,] CalculateLCS(string firstStr, string secondStr)
+        {
+            var firstStrLen = firstStr.Length;
+            var secondStrLen = secondStr.Length;
+            var lcs = new int[firstStrLen + 1, secondStrLen + 1];
+
+            for (int firstStrIdx = 0; firstStrIdx < firstStrLen + 1; firstStrIdx++)
+            {
+                for (int secondStrIdx = 0; secondStrIdx < secondStrLen + 1; secondStrIdx++)
+                {
+                    if (firstStrIdx == 0 || secondStrIdx == 0)
+                    {
+                        lcs[firstStrIdx, secondStrIdx] = 0;
+                        continue;
+                    }
+
+                    var currFirstStrChar = firstStr.ToCharArray()[firstStrIdx - 1];
+                    var currSecondStrChar = secondStr.ToCharArray()[secondStrIdx - 1];
+
+                    var firstStrPrevBest = lcs[firstStrIdx, secondStrIdx - 1];
+                    var secondStrPrevBest = lcs[firstStrIdx - 1, secondStrIdx];
+
+                    var currBest = Math.Max(firstStrPrevBest, secondStrPrevBest);
+                    if (currFirstStrChar == currSecondStrChar)
+                    {
+                        var x = lcs[firstStrIdx - 1, secondStrIdx - 1];
+                        currBest = Math.Max(1 + lcs[firstStrIdx - 1, secondStrIdx - 1], currBest);
+                    }
+
+                    lcs[firstStrIdx, secondStrIdx] = currBest;
+                }
+            }
+
+            return lcs;
+        }
+
+        private static IList<char> ReconstructLCS(int[,] lcs, string firstStr, string secondStr)
+        {
+            var seq = new List<char>();
+
+            var row = lcs.GetLength(0)-1;
+            var col = lcs.GetLength(1)-1;
+
+            char currFirstStrChar;
+            char currSecondStrChar;
+            int currBest;
+
+            while (row > 0 && col > 0)
+            {
+                currFirstStrChar = firstStr.ToCharArray()[row-1];
+                currSecondStrChar = secondStr.ToCharArray()[col-1];
+
+                currBest = lcs[row, col];
+
+                if (currFirstStrChar == currSecondStrChar && currBest == lcs[row - 1, col - 1] + 1)
+                {
+                    seq.Add(currFirstStrChar);
+                    row--;
+                    col--;
+                }
+                else
+                {
+                    var firstStrPrevBest = lcs[row, col - 1];
+                    var secondStrPrevBest = lcs[row - 1, col];
+
+                    if (currBest != secondStrPrevBest && currBest == firstStrPrevBest)
+                    {
+                        col--;
+                    }
+                    else
+                    {
+                        row--;
+                    }
+                }
+            }
+
+            seq.Reverse();
+            return seq;
         }
     }
 }
