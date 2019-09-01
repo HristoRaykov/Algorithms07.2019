940bacef02d777268d44130f0218627288016a0a
hristocr <hristocr@gmail.com>
2019-09-01 17:05:02 +0300

    Solutions for Advanced graph algorithms part I excercises.

---
+++ 09.GraphsAndGraphsAlgorithms-Excercise/AdvancedGraphAlgorithmsI-Excercise/AdvancedGraphAlgorithmsI-Excercise/P02ModifiedKruskalAlgorithm/Program.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace P02ModifiedKruskalAlgorithm
+{
+    internal class Program
+    {
+        private static List<Edge> edges = new List<Edge>();
+
+
+        public static void Main(string[] args)
+        {
+            int nodesCount = int.Parse(Console.ReadLine().Split(' ')[1]);
+            int edgesCount = int.Parse(Console.ReadLine().Split(' ')[1]);
+
+            for (int i = 0; i < edgesCount; i++)
+            {
+                var line = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+                var edge = new Edge(line[0], line[1], line[2]);
+                edges.Add(edge);
+            }
+
+
+            var minimumSpanningForest = KruskalAlgorithm.Kruskal(nodesCount, edges);
+
+            Console.WriteLine("Minimum spanning forest weight: " +
+                              minimumSpanningForest.Sum(e => e.Weight));
+
+//            foreach (var edge in minimumSpanningForest)
+//            {
+//                Console.WriteLine(edge);
+//            }
+        }
+    }
+
+    internal class KruskalAlgorithm
+    {
+        public static List<Edge> Kruskal(int numberOfVertices, List<Edge> edges)
+        {
+            var spanTree = new List<Edge>();
+
+            var parents = new int[numberOfVertices + 1];
+            for (int i = 0; i < parents.Length; i++)
+            {
+                parents[i] = i;
+            }
+
+            var sortedEdges = edges.OrderBy(e => e.Weight);
+
+            foreach (var edge in sortedEdges)
+            {
+                var startNode = edge.StartNode;
+                var endNode = edge.EndNode;
+
+                var startNodeRoot = FindRoot(startNode, parents);
+                var endNodeRoot = FindRoot(endNode, parents);
+
+                if (startNodeRoot != endNodeRoot)
+                {
+                    spanTree.Add(edge);
+                    parents[startNodeRoot] = endNodeRoot;
+                }
+            }
+
+            return spanTree;
+        }
+
+        public static int FindRoot(int node, int[] parent)
+        {
+            // finding root Node
+            var root = node;
+            while (parent[root] != root)
+            {
+                root = parent[root];
+            }
+
+            // path compression setting founded root to be parent of all node we move through
+            while (root != node)
+            {
+                var oldParent = parent[node];
+                parent[node] = root;
+                node = oldParent;
+            }
+
+            return root;
+        }
+    }
+
+    internal class Edge : IComparable<Edge>
+    {
+        public Edge(int startNode, int endNode, int weight)
+        {
+            this.StartNode = startNode;
+            this.EndNode = endNode;
+            this.Weight = weight;
+        }
+
+        public int StartNode { get; set; }
+
+        public int EndNode { get; set; }
+
+        public int Weight { get; set; }
+
+        public int CompareTo(Edge other)
+        {
+            int weightCompared = this.Weight.CompareTo(other.Weight);
+            return weightCompared;
+        }
+
+        public override string ToString()
+        {
+            return $"({this.StartNode} {this.EndNode}) -> {this.Weight}";
+        }
+    }
+}
---
+++ 09.GraphsAndGraphsAlgorithms-Excercise/AdvancedGraphAlgorithmsI-Excercise/AdvancedGraphAlgorithmsI-Excercise/P03MostReliablePath/Program.cs
@@ -0,0 +1,176 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace P03MostReliablePath
+{
+    internal class Program
+    {
+        private static List<int>[] graph;
+
+        private static List<Edge> edges;
+
+        private static int startNode;
+
+        private static int targetNode;
+
+        private static double[] reliablity;
+
+        private static bool[] visited;
+
+        private static int[] previous;
+
+
+        public static void Main(string[] args)
+        {
+            ReadInput();
+
+            Dijkstra();
+
+            var path = ReconstructPath();
+
+            Console.WriteLine($"Most reliable path reliability: {reliablity[targetNode] * 100:F}%");
+            Console.WriteLine(string.Join(" -> ", path));
+        }
+
+
+        private static void Dijkstra()
+        {
+            reliablity[startNode] = 1;
+            previous[startNode] = -1;
+
+            while (true)
+            {
+                var safestNodeParams = GetSafestUnvisitedNode();
+                var safestNode = safestNodeParams.Key;
+                var safestNodeReliability = safestNodeParams.Value;
+                if (safestNode == 0 && safestNodeReliability == 0.00)
+                {
+                    break;
+                }
+
+                visited[safestNode] = true;
+
+                var adjacentNodes = graph[safestNode];
+
+                foreach (var adjacentNode in adjacentNodes)
+                {
+                    if (visited[adjacentNode])
+                    {
+                        continue;
+                    }
+
+                    var adjacentNodeReliability = reliablity[adjacentNode];
+                    var betweenNodesEdge =
+                        edges.Find(e => (e.FirstNode == safestNode && e.SecondNode == adjacentNode) ||
+                                        (e.FirstNode == adjacentNode && e.SecondNode == safestNode));
+                    if (adjacentNodeReliability < safestNodeReliability + betweenNodesEdge.Reliability)
+                    {
+                        reliablity[adjacentNode] = safestNodeReliability * betweenNodesEdge.Reliability;
+                        previous[adjacentNode] = safestNode;
+                    }
+                }
+            }
+        }
+
+        private static KeyValuePair<int, double> GetSafestUnvisitedNode()
+        {
+            int safestNode = 0;
+            double maxReliability = 0.00;
+            for (int currNode = 0; currNode < graph.Length; currNode++)
+            {
+                if (!visited[currNode])
+                {
+                    var currReliability = reliablity[currNode];
+                    if (currReliability > maxReliability)
+                    {
+                        safestNode = currNode;
+                        maxReliability = currReliability;
+                    }
+                }
+            }
+
+            return new KeyValuePair<int, double>(safestNode, maxReliability);
+        }
+        
+        private static List<int> ReconstructPath()
+        {
+            var path = new List<int>();
+
+            var prev = targetNode;
+            while (prev != -1)
+            {
+                path.Add(prev);
+                prev = previous[prev];
+            }
+
+            path.Reverse();
+            return path;
+        }
+
+
+        private static void ReadInput()
+        {
+            var nodeCount = int.Parse(Console.ReadLine().Split( ' ')[1]);
+
+            graph = new List<int>[nodeCount];
+            for (int i = 0; i < graph.Length; i++)
+            {
+                graph[i] = new List<int>();
+            }
+
+            edges = new List<Edge>();
+            reliablity = new double[nodeCount];
+
+            visited = new bool[nodeCount];
+            previous = new int[nodeCount];
+
+            var path = Console.ReadLine().Split(' ').ToArray();
+
+            startNode = int.Parse(path[1]);
+            targetNode = int.Parse(path[3]);
+
+            var edgeCount = int.Parse(Console.ReadLine().Split( ' ')[1]);
+
+            for (int i = 0; i < edgeCount; i++)
+            {
+                var input = Console.ReadLine()
+                    .Split(' ')
+                    .Select(int.Parse)
+                    .ToArray();
+
+                var edge = new Edge(input[0], input[1], input[2] / 100.0);
+                edges.Add(edge);
+                graph[edge.FirstNode].Add(edge.SecondNode);
+                graph[edge.SecondNode].Add(edge.FirstNode);
+            }
+        }
+    }
+
+    internal class Edge : IComparable<Edge>
+    {
+        public int FirstNode { get; set; }
+
+        public int SecondNode { get; set; }
+
+        public double Reliability { get; set; }
+
+        public Edge(int firstNode, int secondNode, double reliability)
+        {
+            this.FirstNode = firstNode;
+            this.SecondNode = secondNode;
+            this.Reliability = reliability;
+        }
+
+        public int CompareTo(Edge other)
+        {
+            int weightCompared = this.Reliability.CompareTo(other.Reliability);
+            return weightCompared;
+        }
+
+        public override string ToString()
+        {
+            return $"({this.FirstNode} {this.SecondNode}) -> {this.Reliability}";
+        }
+    }
+}
