73fe7088fe9258b3589af24a158edd3ef1355da4
hristocr <hristocr@gmail.com>
2019-09-13 21:52:39 +0300

    'Sum to 13' solution added.

---
+++ 13.SolvingPracticalProblems/SolvingPracticalProblems/LabIP02SumTo13/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace LabIP02SumTo13
+{
+    class Program
+    {
+        private static int[] nums;
+
+        private static List<int> possibleSums;
+        
+        private static void Main (string[] args)
+        {
+            nums = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+            possibleSums = new List<int>();
+                
+            CalculateSums(-1, 0);
+
+            if (possibleSums.Contains(13))
+            {
+                Console.WriteLine("Yes");
+            }
+            else
+            {
+                Console.WriteLine("No");
+            }
+
+        }
+    
+        private static void CalculateSums(int idx, int sum)
+        {
+            idx++;
+        
+            if (idx==3)
+            {
+                possibleSums.Add(sum);
+                return;
+            }
+        
+            int num = nums[idx];
+        
+            CalculateSums(idx, sum+num);
+            CalculateSums(idx, sum-num);
+        }
+
+    }
+}
